b6a607b2dd8a8f1025b9c17041e60f645fa22b3d
tukarambahir <tukabahir2001@gmail.com>
2024-10-03 18:14:00 +0530

    rename folder name

---
+++ src/PatternPracticePart2/Pyramid.java
@@ -0,0 +1,16 @@
+package PatternPracticePart2;
+
+public class Pyramid {
+    void printPyramid() {
+        int size = 5;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
